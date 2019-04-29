65fbadbd4d236ec4d6d76f50104b364a40f885b7
Joe Santos Garcia <joesantosgarcia@gmail.com>
2019-04-29 16:27:26 -0400

    Dev-Starter-Kit Ready For Primetime

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const prettyUrl = require('gulp-pretty-url');
 var del = require('del');
 const gulpEdge = require('gulp-edgejs');
 
+// Compiles SCSS To CSS
 gulp.task(
 	'styles',
 	gulp.series(() => {
@@ -29,20 +30,24 @@ gulp.task(
 			.pipe(browserSync.stream());
 	})
 );
+
+// Use Webpack to compile latest Javascript to ES5
+// Webpack on Development Mode
 gulp.task(
 	'webpack:dev',
 	gulp.series(cb => {
-		return exec('npm run dev:webpack', function (err, stdout, stderr) {
+		return exec('npm run dev:webpack', function(err, stdout, stderr) {
 			console.log(stdout);
 			console.log(stderr);
 			cb(err);
 		});
 	})
 );
+// Webpack on Production Mode
 gulp.task(
 	'webpack:prod',
 	gulp.series(cb => {
-		return exec('npm run build:webpack', function (err, stdout, stderr) {
+		return exec('npm run build:webpack', function(err, stdout, stderr) {
 			console.log(stdout);
 			console.log(stderr);
 			cb(err);
@@ -50,9 +55,10 @@ gulp.task(
 	})
 );
 
+// Browser-sync to get live reload and sync with mobile devices
 gulp.task(
 	'browser-sync',
-	gulp.series(function () {
+	gulp.series(function() {
 		browserSync.init({
 			server: './public',
 			notify: false,
@@ -61,9 +67,10 @@ gulp.task(
 	})
 );
 
+// Use Browser Sync With Any Type Of Backend
 gulp.task(
 	'browser-sync-proxy',
-	gulp.series(function () {
+	gulp.series(function() {
 		// THIS IS FOR SITUATIONS WHEN YOU HAVE ANOTHER SERVER RUNNING
 		browserSync.init({
 			proxy: {
@@ -75,7 +82,88 @@ gulp.task(
 	})
 );
 
-// optional this is if you want to create a static website
+// Minimise Your Images
+gulp.task(
+	'imagemin',
+	gulp.series(function buildHTML() {
+		return gulp
+			.src('assets/img/**/*')
+			.pipe(
+				imagemin([
+					imagemin.gifsicle({ interlaced: true }),
+					imagemin.jpegtran({ progressive: true }),
+					imagemin.optipng({ optimizationLevel: 5 }),
+					imagemin.svgo({
+						plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
+					})
+				])
+			)
+			.pipe(gulp.dest('./public/img'));
+	})
+);
+
+// This is your Default Gulp task
+gulp.task(
+	'default',
+	gulp.parallel([
+		gulp.series([
+			'webpack:dev',
+			'styles',
+			function runningWatch() {
+				gulp.watch('./assets/scss/**/*', gulp.parallel('styles'));
+				gulp.watch('./assets/js/**/*', gulp.parallel('webpack:dev'));
+				gulp
+					.watch([
+						'./public/**/*',
+						'./public/*',
+						'!public/js/**/.#*js',
+						'!public/css/**/.#*css'
+					])
+					.on('change', reload);
+			}
+		]),
+		gulp.series(['browser-sync'])
+	])
+);
+
+// This is the task when running on a backend like PHP, PYTHON, GO, etc..
+gulp.task(
+	'watch-proxy',
+	gulp.parallel([
+		gulp.series([
+			'webpack:dev',
+			'styles',
+			function runningWatch() {
+				gulp.watch('./assets/scss/**/*', gulp.parallel('styles'));
+				gulp.watch('./assets/js/**/*', gulp.parallel('webpack:dev'));
+				gulp
+					.watch([
+						'./public/**/*',
+						'./public/*',
+						'!public/js/**/.#*js',
+						'!public/css/**/.#*css'
+					])
+					.on('change', reload);
+			}
+		]),
+		gulp.series(['browser-sync-proxy'])
+	])
+);
+// This is the production build for your app
+gulp.task('build', gulp.series([gulp.parallel(['styles', 'webpack:prod'])]));
+
+/*
+|--------------------------------------------------------------------------
+| Static Site Generator
+|--------------------------------------------------------------------------
+|
+| Run These commands below for static site generator
+|	optional this is if you want to create a static website
+|
+*/
+//
+
+// Generate HTML From Pug or Edge Template Engines
 gulp.task(
 	'views',
 	gulp.series(
@@ -109,7 +197,7 @@ gulp.task(
 		}
 	)
 );
-
+// Delete Your Temp Files
 gulp.task(
 	'cleanTemp',
 	gulp.series(() => {
@@ -121,37 +209,9 @@ gulp.task(
 	})
 );
 
-// gulp.task(
-// 	'pretty-urls',
-// 	gulp.series(function() {
-// 		return gulp
-// 			.src('temp/**/*.html')
-// 			.pipe(prettyUrl())
-// 			.pipe(gulp.dest('public'));
-// 	})
-// );
-
+// Tasks to generate site on development this will also have live reload
 gulp.task(
-	'imagemin',
-	gulp.series(function buildHTML() {
-		return gulp
-			.src('assets/img/**/*')
-			.pipe(
-				imagemin([
-					imagemin.gifsicle({ interlaced: true }),
-					imagemin.jpegtran({ progressive: true }),
-					imagemin.optipng({ optimizationLevel: 5 }),
-					imagemin.svgo({
-						plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
-					})
-				])
-			)
-			.pipe(gulp.dest('./public/img'));
-	})
-);
-
-gulp.task(
-	'default',
+	'static-dev',
 	gulp.parallel([
 		gulp.series([
 			'views',
@@ -177,31 +237,9 @@ gulp.task(
 	])
 );
 
+// this will run your static site for production
 gulp.task(
-	'watch-proxy',
-	gulp.parallel([
-		gulp.series([
-			'webpack:dev',
-			'styles',
-			function runningWatch() {
-				gulp.watch('./assets/scss/**/*', gulp.parallel('styles'));
-				gulp.watch('./assets/js/**/*', gulp.parallel('webpack:dev'));
-				gulp
-					.watch([
-						'./public/**/*',
-						'./public/*',
-						'!public/js/**/.#*js',
-						'!public/css/**/.#*css'
-					])
-					.on('change', reload);
-			}
-		]),
-		gulp.series(['browser-sync-proxy'])
-	])
-);
-
-gulp.task(
-	'build',
+	'static-build',
 	gulp.series([
 		gulp.series(['views', 'cleanTemp']),
 		gulp.parallel(['styles', 'webpack:prod'])
