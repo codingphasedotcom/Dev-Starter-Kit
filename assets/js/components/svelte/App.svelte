cfe8faca18d3eb1c7d6cfbb65fed2ee740bd317c
codingphasedotcom <codingphase@gmail.com>
2019-05-27 01:49:34 -0400

    Update App.svelte

--- assets/js/components/svelte/App.svelte
+++ assets/js/components/svelte/App.svelte
@@ -8,4 +8,39 @@
     }
 </style>
 <Header />
-<h1>Hello {name}!</h1>
+<div class="home">
+    <div class="Aligner">
+        <div class="Aligner-item">
+            <img src="/img/logo.png" />
+            <h1>Dev-Starter-Kit</h1>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="http://starterkit.codingphase.com" target="new">
+                            Documentation
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.codingphase.com" target="new">
+                            CodingPhase.Com
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="version-num">version 4.0.2</div>
+            <br />
+            <a
+                class="github-button"
+                href="https://github.com/codingphasedotcom/Dev-Starter-Kit"
+                data-icon="octicon-star"
+                data-style="mega"
+                data-count-href="/codingphasedotcom/rocky/stargazers"
+                data-count-api="/repos/codingphasedotcom/rocky#stargazers_count"
+                data-count-aria-label="# stargazers on GitHub"
+                aria-label="Star codingphasedotcom/rocky on GitHub"
+            >
+                Star
+            </a>
+        </div>
+    </div>
+</div>
