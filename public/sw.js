2b7e81a5bf4dce4a6a737703a6367160821709d4
codingphasedotcom <codingphase@gmail.com>
2019-08-06 17:54:20 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,28 @@
+//Delete all caches and keep only one
+const cachNameToKeep = 'codingphase-2-2-19';
+
+//Deletion should only occur at the activate event
+self.addEventListener('activate', event => {
+	var cacheKeeplist = [cacheName];
+	event.waitUntil(
+		caches
+			.keys()
+			.then(keyList => {
+				return Promise.all(
+					keyList.map(key => {
+						if (cacheKeeplist.indexOf(key) === -1) {
+							return caches.delete(key);
+						}
+					})
+				);
+			})
+			.then(self.clients.claim())
+	); //this line is important in some contexts
+});
+
 self.addEventListener('install', function(e) {
 	e.waitUntil(
-		caches.open('codingphase').then(function(cache) {
+		caches.open(cachNameToKeep).then(function(cache) {
 			return cache.addAll([
 				'/',
 				'/index.html',
