129ebb9a10e649538d129fbfe74fc461de763637
SidRo <53360670+sidrodev@users.noreply.github.com>
2023-02-15 03:06:58 -0500

    changes occured during npm run watch

--- public/js/dist/FirstComp.js
+++ public/js/dist/FirstComp.js
@@ -1,107 +1,26 @@
-(function(modules) {
-    function webpackJsonpCallback(data) {
-        var chunkIds = data[0];
-        var moreModules = data[1];
-        var executeModules = data[2];
-        var moduleId, chunkId, i = 0, resolves = [];
-        for (;i < chunkIds.length; i++) {
-            chunkId = chunkIds[i];
-            if (Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) resolves.push(installedChunks[chunkId][0]);
-            installedChunks[chunkId] = 0;
-        }
-        for (moduleId in moreModules) if (Object.prototype.hasOwnProperty.call(moreModules, moduleId)) modules[moduleId] = moreModules[moduleId];
-        if (parentJsonpFunction) parentJsonpFunction(data);
-        while (resolves.length) resolves.shift()();
-        deferredModules.push.apply(deferredModules, executeModules || []);
-        return checkDeferredModules();
-    }
-    function checkDeferredModules() {
-        var result;
-        for (var i = 0; i < deferredModules.length; i++) {
-            var deferredModule = deferredModules[i];
-            var fulfilled = true;
-            for (var j = 1; j < deferredModule.length; j++) {
-                var depId = deferredModule[j];
-                if (0 !== installedChunks[depId]) fulfilled = false;
-            }
-            if (fulfilled) {
-                deferredModules.splice(i--, 1);
-                result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
-            }
-        }
-        return result;
-    }
-    var installedModules = {};
-    var installedChunks = {
-        FirstComp: 0
-    };
-    var deferredModules = [];
-    function __webpack_require__(moduleId) {
-        if (installedModules[moduleId]) return installedModules[moduleId].exports;
-        var module = installedModules[moduleId] = {
-            i: moduleId,
-            l: false,
-            exports: {}
-        };
-        modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-        module.l = true;
-        return module.exports;
-    }
-    __webpack_require__.m = modules;
-    __webpack_require__.c = installedModules;
-    __webpack_require__.d = function(exports, name, getter) {
-        if (!__webpack_require__.o(exports, name)) Object.defineProperty(exports, name, {
-            enumerable: true,
-            get: getter
-        });
-    };
-    __webpack_require__.r = function(exports) {
-        if ("undefined" !== typeof Symbol && Symbol.toStringTag) Object.defineProperty(exports, Symbol.toStringTag, {
-            value: "Module"
-        });
-        Object.defineProperty(exports, "__esModule", {
-            value: true
-        });
-    };
-    __webpack_require__.t = function(value, mode) {
-        if (1 & mode) value = __webpack_require__(value);
-        if (8 & mode) return value;
-        if (4 & mode && "object" === typeof value && value && value.__esModule) return value;
-        var ns = Object.create(null);
-        __webpack_require__.r(ns);
-        Object.defineProperty(ns, "default", {
-            enumerable: true,
-            value: value
-        });
-        if (2 & mode && "string" != typeof value) for (var key in value) __webpack_require__.d(ns, key, function(key) {
-            return value[key];
-        }.bind(null, key));
-        return ns;
-    };
-    __webpack_require__.n = function(module) {
-        var getter = module && module.__esModule ? function() {
-            return module["default"];
-        } : function() {
-            return module;
-        };
-        __webpack_require__.d(getter, "a", getter);
-        return getter;
-    };
-    __webpack_require__.o = function(object, property) {
-        return Object.prototype.hasOwnProperty.call(object, property);
-    };
-    __webpack_require__.p = "";
-    var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
-    var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
-    jsonpArray.push = webpackJsonpCallback;
-    jsonpArray = jsonpArray.slice();
-    for (var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
-    var parentJsonpFunction = oldJsonpFunction;
-    deferredModules.push([ "./assets/js/components/FirstComp.js", "vendors~FirstComp" ]);
-    return checkDeferredModules();
-})({
-    "./assets/js/components/FirstComp.js": function(module, __webpack_exports__, __webpack_require__) {
-        "use strict";
-        eval('__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);\nfunction asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }\n\nfunction _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }\n\n\n\n\nclass Layout extends react__WEBPACK_IMPORTED_MODULE_0__["Component"] {\n  constructor() {\n    super();\n\n    this.clickedBtn = () => {};\n\n    this.state = {\n      name: \'Joe\'\n    };\n  }\n\n  test() {\n    return _asyncToGenerator(function* () {})();\n  }\n\n  render() {\n    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "home"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "Aligner"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "Aligner-item"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {\n      src: "/img/logo.png",\n      alt: "codingphase logo"\n    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Dev-Starter-Kit"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "menu"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {\n      href: "http://starterkit.codingphase.com",\n      target: "new"\n    }, "Documentation")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {\n      href: "http://www.codingphase.com",\n      target: "new"\n    }, "CodingPhase.Com")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "version-num"\n    }, "version 4.0.0"))));\n  }\n\n}\n\nconst app = document.getElementById(\'app\');\nreact_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Layout, null), app);\n\n//# sourceURL=webpack:///./assets/js/components/FirstComp.js?');
-    }
-});
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcodingphase_dev_kit"] = self["webpackChunkcodingphase_dev_kit"] || []).push([["FirstComp"],{
+
+/***/ "zpgD":
+/*!*******************************************!*\
+  !*** ./assets/js/components/FirstComp.js ***!
+  \*******************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/prettier-loader/prettier-loader.js):\\nError: Couldn't resolve parser \\\"babylon\\\"\\n    at requireParser (/home/bashenga/Documents/Projects/Codingphase/npm-dev-kits/Dev-Starter-Kit-npm/node_modules/prettier/index.js:6616:15)\\n    at resolveParser (/home/bashenga/Documents/Projects/Codingphase/npm-dev-kits/Dev-Starter-Kit-npm/node_modules/prettier/index.js:7497:16)\\n    at normalize (/home/bashenga/Documents/Projects/Codingphase/npm-dev-kits/Dev-Starter-Kit-npm/node_modules/prettier/index.js:7741:22)\\n    at formatWithCursor2 (/home/bashenga/Documents/Projects/Codingphase/npm-dev-kits/Dev-Starter-Kit-npm/node_modules/prettier/index.js:9006:50)\\n    at /home/bashenga/Documents/Projects/Codingphase/npm-dev-kits/Dev-Starter-Kit-npm/node_modules/prettier/index.js:38176:12\\n    at Object.format (/home/bashenga/Documents/Projects/Codingphase/npm-dev-kits/Dev-Starter-Kit-npm/node_modules/prettier/index.js:38190:12)\\n    at module.exports (/home/bashenga/Documents/Projects/Codingphase/npm-dev-kits/Dev-Starter-Kit-npm/node_modules/prettier-loader/prettier-loader.js:98:31)\");\n\n//# sourceURL=webpack://codingphase-dev-kit/./assets/js/components/FirstComp.js?");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("zpgD"));
+/******/ }
+]);
